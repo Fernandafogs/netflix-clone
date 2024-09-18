c007f8c9d103f1f1f50894b2722c50a43c129da7
Fernanda Fogaça <fernandafogaca@Fernandos-Air.Home>
2024-09-18 12:07:15 +0100

    Display movies data from TMDB API

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,16 +1,32 @@
-import React, { useRef , useEffect} from 'react'
+import React, { useRef , useEffect , useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = ({title, category}) => {
 
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YTM2NTIwOTcwOTBiNzA0MWU5NmNlYzllMTc1YjcxOCIsIm5iZiI6MTcyNjY1NDYzMy4zOTY4ODMsInN1YiI6IjY2ZWFhNGI2YjI5MTdlYjE4MDBiNmM5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zGTJWMYBaohCu0DInVkdnvFmF6nVUTPWdQ5z8U2gmbg'
+    }
+  };
+  
   const handleWheel = (event) => {
     event.preventDefault();
     cardsRef.current.scrollLeft += event.deltaY;
   };
   
   useEffect(()=>{
+    
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:'now_playing'}?language=en-US&page=1`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results))
+    .catch(err => console.error(err));
+  
     cardsRef.current.addEventListener('wheel', handleWheel);
   },[])
 
@@ -18,10 +34,10 @@ const TitleCards = ({title, category}) => {
     <div className='title-cards'>
       <h2>{title?title:'Popular on Netflix'}</h2>
       <div className="cards-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return <div className='card' key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
+            <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+            <p>{card.original_title}</p>
           </div>;
         })}
       </div>
